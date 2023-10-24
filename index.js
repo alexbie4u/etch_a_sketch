94279ce35b1a1ca865a1ce8ca5e6802d6b08d6d6
alexanderbie <bie.alexander@live.dk>
2023-10-24 06:53:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// 16x16 grid of square divs
+
+// Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+
+// 
